16c68b3f6b398d83479973543c103bf0911c83b3
GetHud_0512 <102666293+GetHud0512@users.noreply.github.com>
2022-05-29 12:47:31 +0800

    Update msystem.cpp

--- msystem.cpp
+++ msystem.cpp
@@ -2,36 +2,41 @@
 
 #include "include.h"
 
-#define CHECK(TYPE) if(bad){std::cout << "[system(check):program logic error]"; return TYPE;}
+#define CHECK(TYPE) if(bad){"[system(check):program logic error]"; return TYPE;}
+#define FILE_PATH "C:\\Users\\Public\\Documents\\datas.txt"
+
 
 msystem::msystem()
 {   
-    file_i.open("C:\\Users\\Default\\Documents\\datas.txt",std::fstream::in);
+    file_i.open(FILE_PATH,std::fstream::in);
 
     if(!file_i.is_open())
     {
         file_i.close();
-        std::cout << "Data file open failed." << std::endl;
-        std::cout << "Trying to craet a new data file." << std::endl;
+        Output("Data file opened failed.");
+        Output("Trying to craeted a new data file.");
+        file_o.open(FILE_PATH);
 
-        file_o.open("C:\\Users\\Default\\Documents\\datas.txt");
         if(file_o.is_open())
         {
-            std::cout << "New data file creat successfully." << std::endl;
+            Output("New data file created successfully.");
             file_o.close();
-            file_i.open("C:\\Users\\Default\\Documents\\datas.txt",std::fstream::in);
+            file_i.open(FILE_PATH,std::fstream::in);
         }
         else
         {
-            std::cout << "New data file creat failed." << std::endl;
+            Output("New data file created failed.");
             file_o.close();
-            bad = true;
+
+            this->bad = true;
+
             return;
         }
     }
 
-    bad = false;
-    std::cout << "Data file open successfully." << std::endl;
+    this->bad = false;
+
+    Output("Data file opened successfully.");
 
     std::string line;//文件一行的字符
 
@@ -39,29 +44,51 @@ msystem::msystem()
     {
         content.push_back(line);
     }
-    file_o.open("C:\\Users\\Default\\Documents\\datas.txt");
+
+    file_o.open(FILE_PATH);
+    Output("\
+Set up already, system launched successfully.\n\
+#########################\n\
+#########################\n\
+###ooooooooooooooooooo###\n\
+###ooooooooooooooooooo###\n\
+###ooo.............ooo###\n\
+###ooo.............ooo###\n\
+###ooo....     ....ooo###\n\
+###ooo....     ....ooo###\n\
+###ooo             ooo###\n\
+###ooo             ooo###\n\
+###                   ###\n\
+###                   ###\n\
+Student Admini System ----by progsrc\n\
+Version: v1.1.0 alpha\n\
+Welcome to Student Admini System!\n"
+    );
 
     return;
 }
 
-msystem::msystem(msystem& arg)
+
+msystem::msystem (msystem& arg):bad(true)
 {
     CHECK();
+
+    arg.bad = true;
 
     std::cout << "[system:program logic error]" << std::endl;
 }
 
+
 msystem::~msystem()
 {
-    CHECK();
-
     file_o.close();
-    file_o.open("C:\\Users\\Default\\Documents\\datas.txt",std::ios::trunc);
+    file_o.open(FILE_PATH,std::ios::trunc);
     
     if(!file_o.is_open())
     {
         bad = true;
-        std::cout << "E:Unable to save data into data file." << std::endl;
+        Output("E:Unable to save data into data file.");
+
         return;
     }
 
@@ -76,20 +103,25 @@ msystem::~msystem()
     return;
 }
 
+
 bool msystem::is_bad()
 {
     return bad;
 }
 
+
 void msystem::save()
 {
+    CHECK();
+
     file_o.close();
-    file_o.open("C:\\Users\\Default\\Documents\\datas.txt",std::ios::trunc);
+    file_o.open(FILE_PATH,std::ios::trunc);
     
     if(!file_o.is_open())
     {
         bad = true;
-        std::cout << "E:Unable to save data into data file." << std::endl;
+        Output("E:Unable to save data into data file.");
+
         return;
     }
     
@@ -97,19 +129,25 @@ void msystem::save()
     {
         file_o << content[i] << std::endl;
     }
+
+    return;
 }
+
 
 bool msystem::erase(std::string str)
 {
     if(find(str) < 0)
     {
-        std::cout << "E:Unable to erase the element:the element was not found." << std::endl;
+        Output("E:Unable to erase the element:the element was not found.");
 
         return false;
     }
+
     content.erase(content.begin() + find(str));
+
     return true;
 }
+
 
 int msystem::find(std::string str)const
 {
@@ -124,6 +162,7 @@ int msystem::find(std::string str)const
     return -1;
 }
 
+
 unsigned int msystem::size()const
 {
     CHECK(0);
@@ -131,35 +170,44 @@ unsigned int msystem::size()const
     return content.size();
 }
 
+
 void msystem::Output(const char* str)const
 {
     CHECK();
 
-    std::cout << str << std::endl;
+    std::cout << '[' << GetTime() << ']' << str << std::endl;
 
     return;
 }
+
 
 std::string msystem::Input()const
 {
     CHECK("[program logic error]");
 
     std::string str;
+
     getline(std::cin, str);
+
     return str;
 }
+
 
 std::string msystem::GetTime()const 
 {
     CHECK("[program logic error]");
+
     time_t     now = time(0);
     struct tm  tstruct;
     char       buf[80];
+
     tstruct = *localtime(&now);
+
     strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
 
     return buf;
 }
+
 
 std::string msystem::operator[](unsigned int i)
 {
@@ -168,15 +216,18 @@ std::string msystem::operator[](unsigned int i)
     return content[i];
 }
 
+
 void msystem::operator<<(std::string str)
 {
     CHECK();
+
     if(std::count(content.begin(),content.end(),str))
     {
-        std::cout << "E:Unable to add the element into data:existing element." << std::endl;
+        Output("E:Unable to add the element into data:existing element.");
 
         return;
     }
+
     content.push_back(str);
 
     return;
