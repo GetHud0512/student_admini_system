0b84cb54db3664897cad9fd605f6dee9bade1ca2
GetHud_0512 <102666293+GetHud0512@users.noreply.github.com>
2022-05-29 12:52:23 +0800

    Update msystem.h

--- msystem.h
+++ msystem.h
@@ -20,16 +20,16 @@ public://公有成员-----------------------------------------------------------
     ~msystem();                                     //析构函数
     
     bool            is_bad();                       //返回类的状态
-    void            save();                         //保存函数
-    bool            erase(std::string str);         //删除指定元素函数,成功删除返回true,无指定元素删除失败,返回false
-    int             find(std::string str)const;     //寻找指定元素函数,返回元素的下标,没有就返回-1
-    unsigned int    size()const;                    //获取文件内容元素的个数函数,返回文件内容的元素个数
-    void            Output(const char* str)const;   //输出函数,参数:输出的字符串
-    std::string     Input()const;                   //输入函数,返回输入的字符串
+    bool            erase(std::string str);         //删除文件内容中指定字符串函数,成功删除返回true,无指定元素删除失败,返回false
+    int             find(std::string str)const;     //寻找文件内容中指定元素函数,返回找到的第一个元素的下标,没有找到就返回-1
+    unsigned int    size()const;                    //获取文件内容元素的行数(文件内容容器每行文件字符串作为一个元素)函数,返回文件内容的元素个数
+    void            save();                         //保存修改后的文件内容函数
+    void            Output(const char* str)const;   //输出字符串函数,参数:输出的字符串
+    std::string     Input()const;                   //输入字符串函数,返回输入的字符串
     std::string     GetTime()const;                 //获取时间函数,返回表示当前时间的字符串
  
-    std::string operator [](unsigned int i);        //重载[]运算符,参数:访问文件内容的下标,返回表示文件内容对应下标的字符串
     void        operator <<(std::string str);       //重载<<运算符,参数:输入到文件尾部的字符串
+    std::string operator [](unsigned int i);        //重载[]运算符,参数:访问文件内容的下标,返回表示文件内容对应下标的字符串
     
 protected://保护成员-----------------------------------------------------------------------------------------------------
 };
